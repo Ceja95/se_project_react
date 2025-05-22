18dcff450859ed2832a6eeaece42715f1a38fb1b
Ceja95 <laracejaj@gmail.com>
2025-05-21 18:49:26 -0700

    set up closeActiveModal prop and others for the WTWR app

--- project_react/src/components/Header/Header.jsx
+++ project_react/src/components/Header/Header.jsx
@@ -2,15 +2,14 @@ import "./Header.css";
 import WTWR from "../../images/Logo.svg";
 import avatar from "../../images/avatar.png";
 
-function Header() {
+function Header({ handleAddClick }) {
         const currentDate = new Date().toLocaleString('default', { month: 'long', day: 'numeric' });
-        /*const openClothesModal = !ModalWithForm!.addEventListener("id/class?")*/
 
         return (
             <header className="header">
                 <img className="header__logo" src={WTWR} alt="weather logo" />
                 <p className="header__date-and-location">{currentDate}, Location</p>
-                <button className="header__add-clothes-btn">+Add clothes</button>
+                <button onClick={handleAddClick} type= "button" className="header__add-clothes-btn">+Add clothes</button>
                 <div className="header__user-container">
                     <p className="header__username">Terrence Tegegne</p>
                     <img className="header__avatar" src={avatar} alt="Terrence Tegegne" />
--- project_react/src/components/ModalWithForm/ModalWithForm.jsx
+++ project_react/src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,17 +1,11 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ children, buttonText, title, onClose }) {
-  const modalCloseBtn = document.querySelector(".modal__close");
-  const weatherModalClose = modalCloseBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    
-  });
-
+function ModalWithForm({ children, buttonText, title, activeModal , closeActiveModal}) {
   return (
-    <div className="modal">
+    <div className={`modal ${ activeModal === "add-garment" && "modal_opened" }`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button type="button" className="modal__close"></button>
+        <button  onClick={closeActiveModal} type="button" className="modal__close"></button>
         <form className="modal__form">
           {children}
           <button type="button" className="modal__submit">
