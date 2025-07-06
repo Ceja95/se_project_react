3f1d8991e63d2908e2253234708730de2e80b341
Jose Lara-Ceja <laracejaj@gmail.com>
2025-07-05 19:00:59 -0700

    fixed modal delete confirmation to match the figma design.

--- src/components/ItemModal/ItemModalDelete.jsx
+++ src/components/ItemModal/ItemModalDelete.jsx
@@ -2,13 +2,14 @@ import "./ItemModalDelete.css";
 
 const ItemModalDelete = ({ isOpen, closeActiveModal, handleDeleteItem, itemId }) => {
     return (
-        <div className={`modal ${isOpen && "modal_opened"}`}>
+        <div className={`modal modal__delete ${isOpen && "modal_opened"}`}>
             <div className="modal__content modal__content_type_delete">
-            <button onClick={closeActiveModal} type="button" className="modal__close modal__close_type_delete"></button>
-            <h2 className="modal__delete-title">Delete Item</h2>
-            <p className="modal__delete-text">Are you sure you want to delete this item?</p>
-            <button type="submit" onClick={() => handleDeleteItem(itemId)} className="modal__delete-button">Delete</button>
-            <button type="submit" onClick={closeActiveModal} className="modal__delete-button">Cancel</button>
+                <button onClick={closeActiveModal} type="button" className="modal__close modal__close_type_delete"></button>
+                <p className="modal__delete-title">Are you sure you want to delete this item?
+                    <br />
+                    This action is irreversible.</p>
+                <button type="submit" onClick={() => handleDeleteItem(itemId)} className="modal__delete-button">Yes, delete item</button>
+                <button type="submit" onClick={closeActiveModal} className="modal__delete-button modal__delete-button_cancel">Cancel</button>
             </div>
         </div>
     );
